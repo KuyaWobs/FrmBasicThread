ad6aa62916704ac81a63fa0431eb10a615d49225
buenaventura.298870 <buenaventura.298870@LAB-B-PC06>
2024-10-14 17:43:48 +0800

    Add project files.

---
+++ FrmBasicThread/BasicThread.cs
@@ -0,0 +1,34 @@
+namespace FrmBasicThread
+{
+    public partial class Form1 : Form
+    {
+  
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyThreadClass myThreadClass = new MyThreadClass();
+            Thread ThreadB = new Thread(MyThreadClass.Thread1);
+            Thread ThreadA = new Thread(MyThreadClass.Thread1);
+
+            ThreadA.Name = "Thread Process A = ";
+            ThreadB.Name = "Thread Process B = ";
+
+            Console.WriteLine("-Before Starting Thread-");
+            ThreadA.Start();
+            ThreadB.Start();
+
+            ThreadA.Join();
+            ThreadB.Join();
+
+            Console.WriteLine("-End of Thread-");
+            label1.Text = ("-End of Thread-");
+
+        }
+    }
+}
+
+
---
+++ FrmBasicThread/MyThreadClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrmBasicThread
+{
+    internal class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            {
+                Thread.Sleep(1000);
+
+                for (int LoopCount = 1; LoopCount <= 5; LoopCount++)
+                {
+                    Thread thread = Thread.CurrentThread;
+                    Console.WriteLine("Name of Thread: " + thread.Name + " = " + LoopCount);
+
+                    Thread.Sleep(1500);
+                }   
+            }
+        }
+    }
+}
